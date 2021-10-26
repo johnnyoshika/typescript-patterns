4daf4e31af1662639487d74a8bc6bf35c9a3cbf9
Johnny Oshika <johnnyoshika@gmail.com>
2021-10-26 09:14:01 -0700

    Add Strategy

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ interface Car {
   id: string;
   model: string;
   maxSpeed: number;
+  brakeDistance: number;
 }
 
 interface BaseRecord {
@@ -46,6 +47,7 @@ interface Database<T extends BaseRecord> {
   onAfterSet(listener: Listener<AfterSetEvent<T>>): () => void;
 
   visit(visitor: (item: T) => void): void;
+  selectBest(scoreStrategy: (item: T) => number): T | undefined;
 }
 
 // Factory
@@ -89,6 +91,29 @@ function createDatabase<T extends BaseRecord>() {
     visit(visitor: (item: T) => void): void {
       Object.values(this.data).forEach(visitor);
     }
+
+    // Strategy
+    selectBest(scoreStrategy: (item: T) => number): T | undefined {
+      const init: {
+        max: number;
+        item: T | undefined;
+      } = {
+        max: 0,
+        item: undefined,
+      };
+
+      const best = Object.values(this.data).reduce((best, item) => {
+        const score = scoreStrategy(item);
+        if (score > best.max)
+          return {
+            max: score,
+            item,
+          };
+        else return best;
+      }, init);
+
+      return best.item;
+    }
   }
 
   return InMemoryDatabase;
@@ -104,18 +129,21 @@ CarDB.instance.set({
   id: '1',
   model: 'Ford',
   maxSpeed: 200,
+  brakeDistance: 100,
 });
 
 CarDB.instance.set({
   id: '2',
   model: 'Toyota',
   maxSpeed: 100,
+  brakeDistance: 50,
 });
 
 CarDB.instance.set({
   id: '2',
   model: 'Toyota',
   maxSpeed: 110,
+  brakeDistance: 50,
 });
 
 unsubscribe();
@@ -124,6 +152,15 @@ CarDB.instance.set({
   id: '2',
   model: 'Toyota',
   maxSpeed: 120,
+  brakeDistance: 50,
 });
 
 CarDB.instance.visit(car => console.log(car.model));
+
+const fastest = CarDB.instance.selectBest(car => car.maxSpeed);
+console.log('Fastest', fastest?.model);
+
+const safest = CarDB.instance.selectBest(
+  car => 1000 - car.brakeDistance,
+);
+console.log('Safest', safest?.model);
